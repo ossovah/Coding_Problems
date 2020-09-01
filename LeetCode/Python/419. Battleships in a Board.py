7d46c23116fc4444d4472727d96c92c38e14840d
Osso <ossovh@gmail.com>
2020-09-01 16:31:00 -0700

    Update 419. Battleships in a Board.py

--- LeetCode/Python/419. Battleships in a Board.py
+++ LeetCode/Python/419. Battleships in a Board.py
@@ -1,45 +1,26 @@
-"""
-See the problem description at: https://leetcode.com/problems/battleships-in-a-board/
-"""class Solution:
+class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
-        def find_prev_neighbors(row, col):
-            """
-            Finds the two previously visited neighbors for each node. One at the the
-            same row but at the previous column. The other at the same column but at
-            the previous row.
-            """
-            nei = set()
-            if row == 0 and col == 0: return nei
-            elif row == 0: 
-                nei.add((row, col - 1))
-            elif col == 0: 
-                nei.add((row - 1, col))
-            else:
-                nei.add((row, col - 1))
-                nei.add((row - 1, col))
-        
-            return nei
-        
-        # Count the battleships in the board:
-        if not board: return 0
-        num_batt = 0            # number of battleships
-        rows = len(board)
-        cols = len(board[0])
-        for r in range(rows):
-            for c in range(cols):                
+        """
+        Traverse "board" using two for loops. Suppose we encounter 'X' at row r and column s. We check will check
+        board[r - 1][c] and board[r][c - 1]. If none is 'X', we will add 1 to the number of battleships. We just
+        need to be careful when r = 0 or c = 0.        
+        """
+        if not board: return 0        
+        n_rows = len(board)
+        n_cols = len(board[0])
+        count = 0
+        for r in range(n_rows):
+            for c in range(n_cols):
                 if board[r][c] == 'X':
-                    neighbors = find_prev_neighbors(r, c) # Find previously visited neighbors
-                    if not neighbors: # This is applicable to the first row and first column
-                        num_batt += 1
-                        continue
-                    num_batt_nei = 0
-                    for nei in neighbors:
-                        r_n, c_n = nei
-                        if board[r_n][c_n] == 'X':
-                            num_batt_nei += 1
-                    # If none of the previously visited neighbors were 'X', this is part of a 
-                    # new battleship:
-                    if num_batt_nei == 0:
-                        num_batt += 1
- 
-        return num_batt
+                    if r == 0 and c == 0:
+                        count += 1
+                    elif r == 0:    # i.e., c > 0
+                        if board[0][c - 1] != 'X':
+                            count += 1
+                    elif c == 0:    # i.e., r > 0
+                        if board[r - 1][0] != 'X':
+                            count += 1
+                    else:           # i.e., r,c > 0
+                        if board[r][c - 1] != 'X' and board[r - 1][c] != 'X':
+                            count += 1
+        return count
