5ba10a95817e46982bb8ae7da18b085919b18448
Osso <ossovh@gmail.com>
2020-09-14 12:27:51 -0700

    Update 158. Read N Characters Given Read4 II - Call multiple times.py

--- LeetCode/Python/158. Read N Characters Given Read4 II - Call multiple times.py
+++ LeetCode/Python/158. Read N Characters Given Read4 II - Call multiple times.py
@@ -29,7 +29,7 @@ class Solution(object):
                     self.buf4_chars_yet_to_read -= 1
                     if write_ptr == n: return write_ptr
 
-            n_read_chars = 4    # no read chars using read4. Set to 4 to ensure while is executed given write_ptr < n
+            n_read_chars = 4    # no of read chars using read4. Set to 4 to ensure while is executed given write_ptr < n
             while n_read_chars == 4 and write_ptr < n:  # n_read_chars != 4 means all chars in the file are read
                 n_read_chars = read4(self.buf4)
                 self.buf4_chars_yet_to_read = n_read_chars
